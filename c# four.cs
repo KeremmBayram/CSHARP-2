d1f38dd4fa387fb5727ee2edca9fa86e8545f1ae
KeremmBayram <168360209+KeremmBayram@users.noreply.github.com>
2024-04-28 22:02:41 +0300

    Add files via upload

---
+++ c# four.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dgunu = new DateTime(2009, 10, 23);
+            DateTime simdi = DateTime.Today;
+            int yas = simdi.Year - dgunu.Year;
+            if(dgunu > simdi.AddYears(-yas));
+            yas--;
+
+            Console.WriteLine(yas);
+            Console.ReadLine();
+        }
+    }
+}
