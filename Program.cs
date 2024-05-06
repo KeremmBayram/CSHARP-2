8a6ba9bdfe1da991eb6daead80c7ebe241e3de1a
KeremmBayram <168360209+KeremmBayram@users.noreply.github.com>
2024-05-06 20:06:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+            int tahminedileceksayi = rastgele.Next(0,11);
+            int tahminedilensayi = -1;
+
+
+            while (tahminedileceksayi != tahminedilensayi)
+            {
+                Console.WriteLine("lütfen 0-10 arası bir sayı giriniz");
+                tahminedilensayi = Convert.ToInt32(Console.ReadLine());
+
+                if (tahminedilensayi == tahminedileceksayi)
+                {
+                    Console.WriteLine("tahmininiz doğru");
+
+                }
+                else if (tahminedilensayi < tahminedileceksayi)
+                {
+                    Console.WriteLine("lütfen daha büyük bir sayı giriniz");
+                }
+                else 
+                {
+                    Console.WriteLine("lütfen daha küçük bir sayı giriniz");
+                }
+                
+            }
+            
+            Console.Read();
+        }
+        
+    }
+}
