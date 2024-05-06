78fef28ee4abafcea7ee3f4ba7766af8921fe5e1
KeremmBayram <168360209+KeremmBayram@users.noreply.github.com>
2024-05-06 21:40:04 +0300

    Add files via upload

---
+++ c#.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sifre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogruSifre = "kerem1234";
+            int denemeHakki = 3;
+
+            while (denemeHakki > 0)
+            {
+                Console.Write("Lütfen Şifreyi Girin: ");
+                string girilenSifre = Console.ReadLine();
+
+                if (girilenSifre == dogruSifre)
+                {
+                    Console.WriteLine("Şifre Doğru. Giriş Yapıldı!");
+                    break;
+                }
+                else
+                {
+                    denemeHakki--;
+                    if (denemeHakki == 0)
+                    {
+                        Console.WriteLine("Giriş hakkınız kalmadı. Program sonlandırılıyor.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Yanlış şifre. Kalan deneme hakkınız: {denemeHakki}");
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
