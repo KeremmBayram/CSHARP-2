fc8e9f423c102bb887e3e6acc423f71452e7ee16
KeremmBayram <168360209+KeremmBayram@users.noreply.github.com>
2024-12-05 21:10:05 +0300

    Add files via upload

---
+++ EmlakOtomasyonu10CKeremBayram/EmlakOtomasyonu10CKeremBayram/frmYeniEvEkle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmlakOtomasyonu10CKeremBayram
+{
+    public partial class frmYeniEvEkle : Form
+    {
+        public frmYeniEvEkle()
+        {
+            InitializeComponent();
+        }
+
+        private void frmYeniEvEkle_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
